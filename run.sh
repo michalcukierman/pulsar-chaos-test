004751bc49488849ee6e49538dbc7e3c3d32e4db
Michał Cukierman <michal.cukierman@ds.pl>
2023-08-10 12:58:37 +0200

    Fresh run results with stats

--- run.sh
+++ run.sh
@@ -14,6 +14,9 @@ echo "Creating output dirctory ./out"
 rm -rf ./out
 mkdir ./out
 
+kubectl exec --namespace pulsar -t pulsar-toolset-0 -- bin/pulsar-admin topics partitioned-stats websight/dxp/chaos-topic > out/initial-stats.out
+kubectl exec --namespace pulsar -t pulsar-toolset-0 -- bin/pulsar-admin topics partitioned-stats-internal websight/dxp/chaos-topic > out/initial-stats-internal.out
+
 
 echo "Starting consumer"
 python3 consumer.py &> out/consumer.log &
@@ -23,6 +26,8 @@ python3 producer.py &> out/producer.log &
 PRODUCER_PID=$! 
 echo "Waiting (30s) for the messages to be produced to chaos-topic"
 sleep 30
+kubectl exec --namespace pulsar -t pulsar-toolset-0 -- bin/pulsar-admin topics partitioned-stats websight/dxp/chaos-topic > out/pre-stats.out
+kubectl exec --namespace pulsar -t pulsar-toolset-0 -- bin/pulsar-admin topics partitioned-stats-internal websight/dxp/chaos-topic > out/pre-stats-internal.out
 
 echo "Restarting broker pods"
 kubectl delete pod pulsar-broker-0 -n pulsar
@@ -36,4 +41,6 @@ echo "Killing producer and consumer"
 kill -9 $PRODUCER_PID
 kill -9 $CONSUMER_PID
 
+kubectl exec --namespace pulsar -t pulsar-toolset-0 -- bin/pulsar-admin topics partitioned-stats websight/dxp/chaos-topic > out/post-stats.out
+kubectl exec --namespace pulsar -t pulsar-toolset-0 -- bin/pulsar-admin topics partitioned-stats-internal websight/dxp/chaos-topic > out/post-stats-internal.out
 sh run_verify.sh
